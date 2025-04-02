858291fd53143098b10a599d6d2e8ded44da97db
darya84 <daryalcd1384@gmail.com>
2025-04-03 02:21:33 +0330

    fix(account): edit phone_number to phone

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,20 +2,26 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomerManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, phone_number, password=None, **extra_fields):
+    def create_user(self, email, first_name, last_name, phone, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
-        if not phone_number:
+        if not phone:
             raise ValueError("Users must have an phone number")
         email = self.normalize_email(email)
-        user = self.model(phone_number=phone_number,email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(phone=phone,email=email, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name, phone_number, password=None, **extra_fields):
+    def create_superuser(self, email, first_name,last_name,phone, password=None, **extra_fields):
         extra_fields.setdefault('is_admin', True)
-        user = self.create_user(email, first_name, last_name, phone_number, password, **extra_fields)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        user = self.create_user(email, first_name,last_name,phone, password, **extra_fields)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
         return user
 
 
