ba9ee4095ffb2d6fec45f0e31af23ba02da2cd9d
darya84 <daryalcd1384@gmail.com>
2025-04-02 22:02:21 +0330

    feat(core): - Add LogicalMixin for reusable model logic - Include is_active/is_delete flags - Implement created_at/updated_at auto-tracking

---
+++ core/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class LogicalMixin(models.Model):
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def activate(self):
+        self.is_active = True
+        self.save(update_fields=['is_active'])
+
+    def deactivate(self):
+        self.is_active = False
+        self.save(update_fields=['is_active'])
+
+    def make_delete(self):
+        self.is_delete = True
+        self.save(update_fields=['is_delete'])
+
+    def make_undelete(self):
+        self.is_delete = False
+        self.save(update_fields=['is_delete'])
