024375ea6e543d38cff657c1dd90798b424a1551
darya84 <daryalcd1384@gmail.com>
2025-04-02 22:17:46 +0330

    feat(accounts): add forms.py for creation and change form.

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import Customer
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm Password', widget=forms.PasswordInput)
+    class Meta:
+        model = Customer
+        fields = ('first_name', 'last_name', 'email', 'phone_number')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match")
+        return cd['password2']
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text='u can change password using <a href=\"../password/\">this form</a>.')
+    class Meta:
+        model = Customer
+        fields = ('first_name', 'last_name', 'email', 'phone_number', 'password','last_login')
+
+
