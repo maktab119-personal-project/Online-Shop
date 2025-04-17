8c0834c940a130c1d0d7fa124b1c484eab0c0a40
darya84 <daryalcd1384@gmail.com>
2025-04-17 18:49:13 +0330

    fix(account): Delete duplicate fields and fix the value errors.

--- accounts/managers.py
+++ accounts/managers.py
@@ -13,14 +13,15 @@ class CustomerManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name,last_name,phone, password=None, **extra_fields):
+    def create_superuser(self, email, first_name,phone,last_name, password, **extra_fields):
         extra_fields.setdefault('is_admin', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
-        user = self.create_user(email, first_name,last_name,phone, password, **extra_fields)
-        user.is_admin = True
-        user.is_superuser = True
-        user.is_staff = True
+        user=self.create_user(email, first_name=first_name, phone=phone,last_name=last_name, password=password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
         user.save(using=self._db)
         return user
 
