bcdf324248820a57e7a783231f703ce884e8daf0
darya84 <daryalcd1384@gmail.com>
2025-04-17 18:46:50 +0330

    feat(product): add products admin.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Product, Category
+
+
+# Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id','name','brand_name','category','price','stock','description']
+    ordering = ['id']
+    search_fields = ['name']
+    list_filter = ['category']
+
+
+admin.site.register(Product, ProductsAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'level']
+admin.site.register(Category, CategoryAdmin)
+
